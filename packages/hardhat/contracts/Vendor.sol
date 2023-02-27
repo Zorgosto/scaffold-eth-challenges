623e9f09f7fab26cf32c5ee3631704012fc4ef45
Zorgosto <34967740+Zorgosto@users.noreply.github.com>
2023-02-27 15:33:59 +0100

    add functionality

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,18 +6,36 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event SellTokens(address seller, uint256 amountOfETH, uint256 amountOfTokens);
 
-  YourToken public yourToken;
+    YourToken public yourToken;
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  // ToDo: create a payable buyTokens() function:
+    // create a payable buyTokens() function:
+    uint256 public constant tokensPerEth = 100;
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function buyTokens() public payable {
+        uint256 amount = msg.value * tokensPerEth;
+        yourToken.transfer(msg.sender, amount);
+        emit BuyTokens(msg.sender, msg.value, amount);
+    }
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
+    // create a withdraw() function that lets the owner withdraw ETH
+    function withdraw() public onlyOwner {
+        (bool sent,) = msg.sender.call{value : address(this).balance}("");
+        require(sent, "Failed to send Ether");
+    }
 
+    // create a sellTokens(uint256 _amount) function:
+    function sellTokens(uint256 _amount) public {
+        uint256 ethValue = _amount / 100;
+        yourToken.transferFrom(msg.sender, address(this), _amount);
+        (bool sent,) = msg.sender.call{value : ethValue}("");
+        require(sent, "Failed to send Ether");
+        emit SellTokens(msg.sender, ethValue, _amount);
+    }
 }
